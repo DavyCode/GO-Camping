db9246c776e0e36dcc6c634aeed3aa67e49fd818
Paul Azemoh <pdaswift3009@gmail.com>
2017-05-18 23:09:25 +0100

    Add files via upload

---
+++ Go!Camping/v3/models/campground.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+// campgound  schema
+var GocampingSchema =  mongoose.Schema({
+  name: String,
+  image: String,
+  description : String,
+  comment : [
+    {
+       type : mongoose.Schema.Types.ObjectId,
+       ref : "Comment"
+    }
+  ]
+});
+
+module.exports = mongoose.model("Campgound", GocampingSchema);
+
