42a762a377b20d93cbf2597db425cb72d7eaaed9
Azemoh  paul <pdaswift3009@gmail.com>
2017-06-05 07:35:31 +0100

    handle  flash err

--- Go!Camping/v11/routes/index.js
+++ Go!Camping/v11/routes/index.js
@@ -24,7 +24,7 @@ router.post('/register', (req, res) => {
     var newUser = new User({ username: req.body.username });
     User.register(newUser, req.body.password, (err, user) => {
         if (err) {
-            req.flash('error', err);
+            req.flash('error', err.message);
             return res.render('register');
         }
         passport.authenticate('local')(req, res, () => {
